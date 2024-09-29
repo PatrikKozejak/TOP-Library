95718ca195508cba8e6e15f5bb948583316bce7d
Patrik Kozejak <patrick.kozejak@gmail.com>
2024-09-29 21:16:11 +0200

    Implement the use of classes

--- js/script.js
+++ js/script.js
@@ -1,5 +1,3 @@
-const myLibrary = [];
-
 const dialog = document.querySelector("dialog");
 const showButton = document.querySelector(".add");
 const closeButton = document.querySelector(".close-modal");
@@ -20,12 +18,58 @@ closeButton.addEventListener("click", () => {
 
 createButton.addEventListener("click", createButtonClick, false);
 
-function Book(title, author, pages, read) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
+class Book {
+  constructor(title, author, pages, read) {
+    this._title = title;
+    this._author = author;
+    this._pages = pages;
+    this._read = read;
+  }
+
+  get title() {
+    return this._title;
+  }
+
+  get author() {
+    return this._author;
+  }
+
+  get pages() {
+    return this._pages;
+  }
+
+  get read() {
+    return this._read;
+  }
+
+  set read(status) {
+    this._read = status;
+  }
 }
+
+class Library {
+  constructor() {
+    this._books = [];
+  }
+
+  get getBooks() {
+    return this._books;
+  }
+
+  addBookToLibrary(book) {
+    this._books.push(book);
+  }
+
+  getBookByIndex(index) {
+    return this._books[index];
+  }
+
+  deleteBook(bookIndex) {
+    this._books.splice(bookIndex, 1);
+  }
+}
+
+let myLibrary = new Library();
 
 function createButtonClick(event) {
   event.preventDefault();
@@ -36,7 +80,7 @@ function createButtonClick(event) {
     bookPages.value,
     bookRead.checked
   );
-  myLibrary.push(createdBook);
+  myLibrary.addBookToLibrary(createdBook);
   form.reset();
   dialog.close();
   displayBooks();
@@ -48,12 +92,13 @@ let book2 = new Book("Klub Zahad", "neznam", 123, true);
 
 let book3 = new Book("Hobbit", "Tolkien", 1250, false);
 
-addBookToLibrary(book1);
-addBookToLibrary(book2);
-addBookToLibrary(book3);
+myLibrary.addBookToLibrary(book1);
+myLibrary.addBookToLibrary(book2);
+myLibrary.addBookToLibrary(book3);
 
-function addBookToLibrary(book) {
-  myLibrary.push(book);
+function deleteBook(bookIndex) {
+  myLibrary.deleteBook(bookIndex);
+  displayBooks();
 }
 
 function createDeleteButton(bookIndex) {
@@ -75,13 +120,9 @@ function createReadCheckbox(bookIndex) {
   return readCheckbox;
 }
 
-function deleteBook(bookIndex) {
-  myLibrary.splice(bookIndex, 1);
-  displayBooks();
-}
-
 function changeReadStatus(bookIndex) {
-  myLibrary[bookIndex].read = !myLibrary[bookIndex].read;
+  myLibrary.getBookByIndex(bookIndex).read =
+    !myLibrary.getBookByIndex(bookIndex).read;
   displayBooks();
 }
 
@@ -89,26 +130,27 @@ function displayBooks() {
   let tableBody = document.querySelector(".tableBody");
   let newTbody = document.createElement("tbody");
   newTbody.setAttribute("class", "tableBody");
-  for (let book in myLibrary) {
-    let currentBook = myLibrary[book];
+  let bookIndex = 0;
+  for (let book of myLibrary.getBooks) {
+    console.log(book);
     let newRow = newTbody.insertRow(-1);
 
     let titleCell = newRow.insertCell(0);
-    let newTitle = document.createTextNode(currentBook.title);
+    let newTitle = document.createTextNode(book.title);
     titleCell.appendChild(newTitle);
 
     let authorCell = newRow.insertCell(1);
-    let newAuthor = document.createTextNode(currentBook.author);
+    let newAuthor = document.createTextNode(book.author);
     authorCell.appendChild(newAuthor);
 
     let pagesCell = newRow.insertCell(2);
-    let newPages = document.createTextNode(currentBook.pages);
+    let newPages = document.createTextNode(book.pages);
     pagesCell.appendChild(newPages);
 
     let readCell = newRow.insertCell(3);
     readCell.setAttribute("class", "checkbox-cell");
-    let newReadCheckbox = createReadCheckbox(book);
-    newReadCheckbox.checked = currentBook.read;
+    let newReadCheckbox = createReadCheckbox(bookIndex);
+    newReadCheckbox.checked = book.read;
     readCell.appendChild(newReadCheckbox);
 
     let readLabel = document.createElement("label");
@@ -127,7 +169,8 @@ function displayBooks() {
     readLabel.appendChild(offSpan);
 
     let deleteCell = newRow.insertCell(4);
-    deleteCell.appendChild(createDeleteButton(book));
+    deleteCell.appendChild(createDeleteButton(bookIndex));
+    bookIndex++;
   }
   tableBody.parentNode.replaceChild(newTbody, tableBody);
 
